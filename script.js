22d5e81b2e1d20d954a00d5d79d78e3cc305f1e9
hemantjodhani <jodhani.hemant@gmail.com>
2023-04-27 18:00:36 +0530

    Icon functionality added

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ $(document).ready(function(){
         const api_key = "74e5d425b52f8580fd8f08d7bb0459a8";
         var city = $(".city-input").val() || "Delhi";
         var api_url = "https://api.openweathermap.org/data/2.5/weather?q="+city+ "&appid=" + api_key + "&units=metric" ;
-    
         $.get(api_url)
-    
             .done(function(data){
+                var current_icon = "https://openweathermap.org/img/wn/"+data.weather[0].icon+".png";
                 $(".discription").text(data.weather[0].description)
                 $(".city-name").text(data.name)
                 $(".exact-temp").text(data.main.temp + " C")
                 $(".wind").text(data.wind.speed + " km/h")
                 $(".humidity").text(data.main.humidity)
                 $(".visibility").text(data.visibility)
+                $(".weather-icon").attr("src", current_icon);
             });
     }
     data_appender()
