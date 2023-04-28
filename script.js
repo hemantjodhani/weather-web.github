0ce253f03aa936bfc7304b919c660e407563ac2b
hemantjodhani <jodhani.hemant@gmail.com>
2023-04-28 16:51:55 +0530

    api changed

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
+const API_KEY = "74e5d425b52f8580fd8f08d7bb0459a8";
 $(document).ready(function(){
 
-    var  geolocation = ""
+    navigator.geolocation.getCurrentPosition(function(pos) {
+        const crd = pos.coords;
+
+        var latitude = crd.latitude;
+        var longitude = crd.longitude;
+        var city_fetcher_api = "http://api.openweathermap.org/geo/1.0/reverse?lat="+latitude+"&lon="+longitude+"&appid="+API_KEY;
+        $.get(city_fetcher_api)
+        .done(function(lat_long_city){
+            var city = lat_long_city[0].name;
+        
+            data_appender(city)
+        });
+    });
 
 
-    function ip_city_generator(){
-
-        const api = "http://ip-api.com/json/?fields=61439"
-         $.get(api)
-            .done(function(ip_adress){
-                // console.log(ip_adress.city)
-                geolocation = ip_adress.city
-                data_appender()
-            })
-    }
-    
-    function data_appender(){
-        const api_key = "74e5d425b52f8580fd8f08d7bb0459a8";
-        var city = $(".city-input").val() || geolocation ;
-        var api_url = "https://api.openweathermap.org/data/2.5/weather?q="+city+ "&appid=" + api_key + "&units=metric" ;
+    function data_appender(city){
+         city = $(".city-input").val() || city ;
+        var api_url = "https://api.openweathermap.org/data/2.5/weather?q="+city+ "&appid=" + API_KEY + "&units=metric" ;
         $.get(api_url)
         .done(function(data){
             var current_icon = "https://openweathermap.org/img/wn/"+data.weather[0].icon+".png";
@@ -28,18 +29,20 @@ $(document).ready(function(){
             $(".humidity").text(data.main.humidity)
             $(".visibility").text(data.visibility)
             $(".weather-icon").attr("src", current_icon);
-        });
+        })
+        .fail(function(){
+            alert("Sorry, couldn't load your current location");
+            $(".weather-section").hide()
+        })
     }
-
-    ip_city_generator()
-
     $(".inputs svg").click(function(){
         data_appender()
+        $(".weather-section").show()
     });
     $(".inputs").keydown(function(e){
         if(e.keyCode==13){
             data_appender()
+            $(".weather-section").show()
         }
     });
-    
 });
